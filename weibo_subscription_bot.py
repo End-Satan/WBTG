532829327240d3529252785d6af17d1511c2450c
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-14 16:31:01 -0700

    commit

--- weibo_subscription_bot.py
+++ weibo_subscription_bot.py
@@ -28,7 +28,7 @@ def shouldProcess(channel, card, key):
 # a hack, we can fetch the single item again, but weibo will 
 # be unhappy about the frequent calls
 def removeSeeMore(result): 
-	print(result.cap[-10:])
+	print(result.cap[-40:])
 	result.cap = result.cap.rstrip('全文')
 
 def process(key):
