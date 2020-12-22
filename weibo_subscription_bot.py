071df9fbf386db9bd1c84d90897d0ece52622162
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-22 07:32:26 -0800

    commit

--- weibo_subscription_bot.py
+++ weibo_subscription_bot.py
@@ -3,7 +3,7 @@
 
 from telegram_util import log_on_fail, removeOldFiles, getLogStr, isInt
 import album_sender
-from db import subscription, existing, weibo_name
+from db import subscription, existing
 import threading
 import weibo_2_album
 from command import setupCommand
@@ -13,17 +13,12 @@ import random
 from filter import passFilter
 import time
 
-processed_channels = set()
-
 def shouldProcess(channel, card, key):
-	if channel.id in processed_channels:
-		return False
 	if not passFilter(channel, card, key):
 		return False
 	whash = weiboo.getHash(card) + str(channel.id)
 	if not existing.add(whash):
 		return False
-	processed_channels.add(channel.id)
 	return True
 
 # a hack, we can fetch the single item again, but weibo will 
@@ -40,10 +35,6 @@ def removeSeeMore(result):
 def process(key):
 	channels = subscription.channels(tele.bot, key)
 	search_result = weiboo.search(key, force_cache=True)
-	if isInt(key): # backfill, may able to be removed
-		result = weiboo.searchUser(key)
-		if result:
-			weibo_name.update(result[0], result[1])
 	if not search_result:
 		print('no search result', key)
 	for url, card in search_result:
@@ -51,9 +42,7 @@ def process(key):
 		for channel in channels:
 			if not shouldProcess(channel, card, key):
 				continue
-			print(url, channel.id, channel.username)
 			if not result:
-				time.sleep(60)
 				result = weibo_2_album.get(url, card['mblog'])
 				removeSeeMore(result)
 			try:
@@ -61,16 +50,17 @@ def process(key):
 			except Exception as e:
 				debug_group.send_message(getLogStr(channel.username, channel.id, url, e))
 				raise e
+		if result:
+			return
 
 @log_on_fail(debug_group)
 def loopImp():
 	removeOldFiles('tmp', day=0.1) # video could be very large
-	global processed_channels 
-	processed_channels = set()
 	for key in subscription.subscriptions():
 		if random.random() > 0.01:
 			continue
 		process(key)
+		return
 
 def loop():
 	loopImp()
