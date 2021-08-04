5ec7c39005650b1ef4c7d96601800eee411754d3
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-08-04 11:30:27 -0700

    commit

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from db import subscription
+
 def oneTimeCleanSubscriber():
 	for chat_id in list(subscription.sub.keys()):
 		try:
@@ -6,3 +8,17 @@ def oneTimeCleanSubscriber():
 		except:
 			del subscription.sub[chat_id]
 	subscription.save()
+
+def removeNoResult():
+	items = {}
+	with open('tmp_no_result') as f:
+		for line in f:
+			item = line.strip()
+			items[item] = items.get(item, 0) + 1
+	for item, count in items.items():
+		if count < 3:
+			continue
+		print(item)
+
+if __name__ == '__main__':
+	removeNoResult()
