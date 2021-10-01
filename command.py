8bd029f8b1e0c997819724d84894b791b2265f3d
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-10-01 04:58:42 -0700

    commit

--- command.py
+++ command.py
@@ -3,11 +3,12 @@ from telegram_util import log_on_fail, splitCommand, tryDelete, autoDestroy
 from common import debug_group
 from db import subscription, blocklist, popularlist, scheduled_key
 
-core_channels_ids = set([-1001496977825, -1001374366482, -1001340272388, -1001326932731, -1001598520359, -1001570955777, -1001553483809, -1001168883038])
+core_channels_ids = set([-1001374366482, -1001340272388, -1001326932731, -1001598520359, -1001570955777, -1001553483809, -1001168883038])
+additional_abl_command_support_channel = set([-1001496977825, -1001316672281])
 
 @log_on_fail(debug_group)
 def handleAdmin(msg):
-	if msg.chat.id not in core_channels_ids:
+	if msg.chat.id not in (core_channels_ids | additional_abl_command_support_channel):
 		return
 	command, text = splitCommand(msg.text)
 	if not text:
